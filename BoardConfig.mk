777511e685e8f15af8e238c485e929a61ecafd35
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 15:31:33 +0530

    Add OrangeFox/TWRP recovery configuration options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,3 +23,22 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/oneplus/CPH2487/BoardConfigVendor.mk
+
+
+# OrangeFox/TWRP Recovery Flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_HAS_MTP := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE_DECRYPT := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_TOOLBOX := true
+TW_EXCLUDE_APEX := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_NO_SCREEN_BLANK := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 1200
+
+# For A/B devices
+TW_INCLUDE_REPACKTOOLS := true
