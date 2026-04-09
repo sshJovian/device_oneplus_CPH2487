cde77ee191cb4cbc9b6fdcc8b63ffb23cd5d7467
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-10 02:30:41 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,14 @@
 #
-# Copyright (C) 2023 The OrangeFox Recovery Project
+# Copyright (C) 2026 The OrangeFox Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Specify the local path for the product makefiles
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_CPH2487.mk
 
+# List all possible lunch commands for this device
 COMMON_LUNCH_CHOICES := \
     fox_CPH2487-user \
     fox_CPH2487-userdebug \
