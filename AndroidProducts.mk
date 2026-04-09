8844c96a1a6ded70d7c5b3ca07bbee79ac627c74
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 19:26:55 +0530

    Update lunch choices to include fox_CPH2487 variants

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_CPH2487.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_CPH2487-user \
-    lineage_CPH2487-userdebug \
-    lineage_CPH2487-eng
+    fox_CPH2487-user \
+    fox_CPH2487-userdebug \
+    fox_CPH2487-eng
