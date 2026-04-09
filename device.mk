87c6c220217876f2cf9711eca25b667c2e95afd3
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 19:11:48 +0530

    Modify device.mk for CPH2487 configuration
    Updated product name and added shipping API level for Android 13.

--- device.mk
+++ device.mk
@@ -1,27 +1,19 @@
-# Inherit from common
 $(call inherit-product, device/oneplus/sm8475-common/common.mk)
 
-# Device specific
 PRODUCT_DEVICE := CPH2487
-PRODUCT_NAME := omni_CPH2487
+PRODUCT_NAME := fox_CPH2487
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 11R
 PRODUCT_MANUFACTURER := OnePlus
 
-# Recovery
-PRODUCT_COPY_FILES += \
-    device/oneplus/CPH2487/recovery/root/system/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/fstab.qcom
-
 # A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    vendor_boot \
-    system \
-    vendor \
-    product \
-    system_ext \
-    odm
-
-# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping API (IMPORTANT for Android 13+)
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Recovery
+PRODUCT_PACKAGES += \
+    fastbootd \
+    linker.vendor_ramdisk \
+    android.hardware.boot@1.2-impl.recovery
