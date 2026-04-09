ef05daaf18d9ec27db9baeeafa96b49062084692
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 17:32:03 +0530

    Clean up fox_CPH2487.mk by removing unused variables
    Removed unnecessary product manufacturer and various flags.

--- fox_CPH2487.mk
+++ fox_CPH2487.mk
@@ -7,7 +7,6 @@ PRODUCT_RELEASE_NAME := udon
 PRODUCT_DEVICE := udon
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2487
-PRODUCT_MANUFACTURER := OnePlus
 
 # Inherit OrangeFox base
 $(call inherit-product-if-exists, vendor/recovery/orangefox.mk)
@@ -19,13 +18,8 @@ TW_HAS_MTP := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE_DECRYPT := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
-TW_USE_TOOLBOX := true
-TW_EXCLUDE_APEX := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_NO_SCREEN_BLANK := true
 
 # A/B + Android 16 flags
 OF_AB_DEVICE_WITH_RECOVERY_PARTITION := true
 OF_VIRTUAL_AB_DEVICE := true
-OF_FIX_DECRYPTION_ON_DATA_MEDIA := true
-OF_PATCH_AVB20 := true
+
