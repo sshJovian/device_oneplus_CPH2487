27fe60c988a74b45d380bd7b4591e9100061558e
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 16:46:31 +0530

    Add vendorsetup for OnePlus 11R with Android 16 support

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+# OrangeFox vendorsetup for OnePlus 11R (udon) - Android 16 ready
+
+export FOX_BUILD_TYPE="Unofficial"
+export FOX_VERSION="R11.3-Unofficial-udon"
+export FOX_AB_DEVICE=true
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=true
+export OF_VIRTUAL_AB_DEVICE=true
+export TW_INCLUDE_CRYPTO=true
+export TW_INCLUDE_FBE_DECRYPT=true
+export TW_INCLUDE_FBE_METADATA_DECRYPT=true
+export OF_FIX_DECRYPTION_ON_DATA_MEDIA=true
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=true
+export OF_USE_MAGISKBOOT=true
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE=true
+export TW_NO_REBOOT_BOOTLOADER=true
+export TW_HAS_DOWNLOAD_MODE=true
