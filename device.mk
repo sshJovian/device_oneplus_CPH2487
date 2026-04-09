aa4616e4fa33a659366a9192470e6d282b1348a0
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 19:26:18 +0530

    Update device.mk for OnePlus CPH2487 configuration

--- device.mk
+++ device.mk
@@ -1,22 +1,136 @@
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8475-common
 $(call inherit-product, device/oneplus/sm8475-common/common.mk)
 
-PRODUCT_DEVICE := CPH2487
-PRODUCT_NAME := fox_CPH2487
+# Device path
+DEVICE_PATH := device/oneplus/CPH2487
+
+# API level for Android 16
+PRODUCT_SHIPPING_API_LEVEL := 36
+PRODUCT_TARGET_API_LEVEL := 36
+PRODUCT_SDK_ADDON_NAME := fox_CPH2487
+
+# Device properties
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 11R
 PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_NAME := fox_CPH2487
+PRODUCT_DEVICE := CPH2487
+PRODUCT_MODEL := CPH2487
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-# A/B
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2772
+TARGET_SCREEN_WIDTH := 1240
 
-# Shipping API (IMPORTANT for Android 13+)
-PRODUCT_SHIPPING_API_LEVEL := 33
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
-# Recovery
+# Media
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(DEVICE_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+
+# Overlays
 PRODUCT_PACKAGES += \
-    fastbootd \
-    linker.vendor_ramdisk \
-    android.hardware.boot@1.2-impl.recovery
+    FrameworksResTarget \
+    SystemUIResTarget \
+    SettingsProviderResTarget \
+    SettingsResTarget
 
-TARGET_PREBUILT_KERNEL := device/oneplus/CPH2487-prebuilt/kernel
-BOARD_PREBUILT_DTBOIMAGE := device/oneplus/CPH2487-prebuilt/dtbo.img
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(DEVICE_PATH)/overlay/packages/apps/CarrierConfig
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+# Vibrator
+$(call inherit-product, device/oneplus/sm8475-common/vibrator/vibrator.mk)
+
+# Audio
+$(call inherit-product, device/oneplus/sm8475-common/audio/audio.mk)
+
+# Camera
+$(call inherit-product, device/oneplus/sm8475-common/camera/camera.mk)
+
+# NFC
+$(call inherit-product, device/oneplus/sm8475-common/nfc/nfc.mk)
+
+# Sensors
+$(call inherit-product, device/oneplus/sm8475-common/sensors/sensors.mk)
+
+# Touch
+$(call inherit-product, device/oneplus/sm8475-common/touch/touch.mk)
+
+# Wi-Fi
+$(call inherit-product, device/oneplus/sm8475-common/wifi/wifi.mk)
+
+# Bluetooth
+$(call inherit-product, device/oneplus/sm8475-common/bluetooth/bluetooth.mk)
+
+# Display
+$(call inherit-product, device/oneplus/sm8475-common/display/display.mk)
+
+# Media
+$(call inherit-product, device/oneplus/sm8475-common/media/media.mk)
+
+# Telephony
+$(call inherit-product, device/oneplus/sm8475-common/telephony/telephony.mk)
+
+# Permissions
+$(call inherit-product, device/oneplus/sm8475-common/permissions/permissions.mk)
+
+# Power
+$(call inherit-product, device/oneplus/sm8475-common/power/power.mk)
+
+# Keyhandler
+$(call inherit-product, device/oneplus/sm8475-common/KeyHandler/KeyHandler.mk)
+
+# Include OrangeFox vendor
+$(call inherit-product, vendor/fox/product/fox.mk)
+
+# OrangeFox version
+FOX_VERSION := R12.1_CPH2487
