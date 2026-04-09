6042314b888fe51a9c5f8eda1cc0015f15b7ca9a
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 19:08:13 +0530

    Add lunch combo for CPH2487
    Added lunch combo options for CPH2487.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,5 @@
 # OrangeFox vendorsetup for OnePlus 11R (udon) - Android 16 ready
 
-export FOX_BUILD_TYPE="Unofficial"
-export FOX_VERSION="R11.3-Unofficial-udon"
-export FOX_AB_DEVICE=true
-export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=true
-export OF_VIRTUAL_AB_DEVICE=true
-export TW_INCLUDE_CRYPTO=true
-export TW_INCLUDE_FBE_DECRYPT=true
-export TW_INCLUDE_FBE_METADATA_DECRYPT=true
-export OF_FIX_DECRYPTION_ON_DATA_MEDIA=true
-export TW_NO_REBOOT_BOOTLOADER=true
-export TW_HAS_DOWNLOAD_MODE=true
+
+add_lunch_combo fox_CPH2487-eng
+add_lunch_combo fox_CPH2487-userdebug
