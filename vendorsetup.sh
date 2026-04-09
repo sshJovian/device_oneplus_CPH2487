13a7d952dfa4bbb6f4374bcf095083d9e6a0b3fb
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-09 19:32:19 +0530

    Update vendorsetup.sh for OnePlus 11R lunch combos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,10 @@
-# OrangeFox vendorsetup for OnePlus 11R (udon) - Android 16 ready
+#!/bin/bash
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-
-add_lunch_combo fox_CPH2487-eng
+add_lunch_combo fox_CPH2487-user
 add_lunch_combo fox_CPH2487-userdebug
+add_lunch_combo fox_CPH2487-eng
