cba35d291a1b3fa3e0d5e3f5decc1d59046cec69
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-10 23:43:11 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,13 +3,5 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# List of all product makefiles for this device
-COMMON_LUNCH_CHOICES := \
-    fox_CPH2487-user \
-    fox_CPH2487-userdebug \
-    fox_CPH2487-eng
-
-# Legacy support for older build systems
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_CPH2487.mk
