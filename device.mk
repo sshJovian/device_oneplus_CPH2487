351e70066340580879a73f980f92ef19419815ce
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-11 02:55:23 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,19 +8,19 @@
 $(call inherit-product, device/oneplus/sm8475-common/common.mk)
 
 # Device path
-DEVICE_PATH := device/oneplus/CPH2487
+DEVICE_PATH := device/oneplus/udon
 
 # API level for Android 16
 PRODUCT_SHIPPING_API_LEVEL := 36
 PRODUCT_TARGET_API_LEVEL := 36
-PRODUCT_SDK_ADDON_NAME := fox_CPH2487
+PRODUCT_SDK_ADDON_NAME := fox_udon
 
 # Device properties
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_NAME := fox_CPH2487
-PRODUCT_DEVICE := CPH2487
-PRODUCT_MODEL := CPH2487
+PRODUCT_NAME := fox_udon
+PRODUCT_DEVICE := udon
+PRODUCT_MODEL := udon
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 # Boot animation
@@ -31,4 +31,4 @@ TARGET_SCREEN_WIDTH := 1240
 $(call inherit-product, vendor/fox/product/fox.mk)
 
 # OrangeFox version
-FOX_VERSION := R12.1_CPH2487
+FOX_VERSION := R12.1_udon
