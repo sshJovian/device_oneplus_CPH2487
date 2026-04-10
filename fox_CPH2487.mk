3484464546e88776acb361016d83fc28f439c561
sshJovian <96281738+sshJovian@users.noreply.github.com>
2026-04-11 00:01:52 +0530

    Update fox_CPH2487.mk

--- fox_CPH2487.mk
+++ fox_CPH2487.mk
@@ -1,8 +1,8 @@
-# OrangeFox Recovery for OnePlus 11R (udon / CPH2487)
+# OrangeFox Recovery for OnePlus 11R (udon / CPH2487) - Fixed
 
-$(call inherit-product, device/oneplus/sm8475-common/udon.mk)
+$(call inherit-product, device/oneplus/sm8475-common/common.mk)
 
-# Device info
+# Device identifiers
 PRODUCT_RELEASE_NAME := udon
 PRODUCT_DEVICE := udon
 PRODUCT_BRAND := OnePlus
@@ -12,7 +12,7 @@ PRODUCT_MANUFACTURER := OnePlus
 # Inherit OrangeFox
 $(call inherit-product-if-exists, vendor/recovery/orangefox.mk)
 
-# Recovery configuration
+# Recovery flags
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_HAS_MTP := true
@@ -23,7 +23,8 @@ TW_USE_TOOLBOX := true
 TW_EXCLUDE_APEX := true
 TW_INCLUDE_REPACKTOOLS := true
 
-# A/B and 64-bit fixes (very important)
+# A/B + 64-bit fixes
 OF_AB_DEVICE_WITH_RECOVERY_PARTITION := true
 OF_VIRTUAL_AB_DEVICE := true
 TARGET_SUPPORTS_64_BIT_APPS := true
+OF_FIX_DECRYPTION_ON_DATA_MEDIA := true
